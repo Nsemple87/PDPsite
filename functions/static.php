718d0e165483c98721788e32c45ba151420b4151
Nicole <ns21549n@pace.edu>
2014-01-22 23:09:03 -0500

    Navigation bar updates-transparent

--- functions/static.php
+++ functions/static.php
@@ -6,11 +6,13 @@ $css = '<!-- CSS --><link type="text/css" rel="stylesheet" href="css/resetstyles
 	   <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/><!-- CSS -->';
 
 $navBar = '<!-- Navigation bar -->
-		<div class= "fullpage" id="header">
-		<nav><table><tr><td><a href="index.php"><img src="images/cup.gif" width="87" height="100"></a></td>
-			<td><a href="project.php">the Project</a></td>
-			<td><a href="people.php">the People</a></td>
-			<td><a href="prototype.php">the Prototype</a></td>
-			<td><a href="press.php">the Press</a></td>
-		</tr></table></nav></div>';
+		<div class= "fullpage" id="navigation">
+		<nav>		
+		<a href="index.php"><img src="images/cup.gif" width="87" height="100"></a></td>
+		<a href="project.php">the Project</a></td>
+		<a href="people.php">the People</a></td>
+		<a href="prototype.php">the Prototype</a></td>
+		<a href="press.php">the Press</a></td>
+		</nav>
+		</div>';
 ?>
--- index.php
+++ index.php
@@ -3,18 +3,25 @@ require_once 'core/init.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php echo $css?>
 <title>Home</title>
 </head>
+
 <body>
 <?php echo $navBar ?>
+<div class= "fullpage" id="header">
+</div>
 <div class= "fullpage" id="midway">
 </div>
+
 <div class= "fullpage" id="midway1">
 </div>
+
 <div class="fullpage" id="footer">
 </div>
 </body>
+
 </html>
