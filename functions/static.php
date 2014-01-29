684671e0a29aab28b3a45bf5e07213ea727c7a1c
Shane <skirk@pace.edu>
2014-01-28 21:01:37 -0500

    added more images

--- functions/static.php
+++ functions/static.php
@@ -14,7 +14,7 @@ $header = '<!-- Navigation bar -->
 								<td>&nbsp;</td>
 							</tr>
 							<tr>
-								<td><a class="logo" href="index.php"><img src="images/logo.gif" width="190" height="105"></a></td>
+								<td><a class="logo" href="index.php"><img src="images/icons/logo.gif" width="190" height="105"></a></td>
 			                    <td><a class="menu" href="project.php">the Project</a></td>
 			                    <td><a class="menu" href="people.php">the People</a></td>
 			                    <td><a class="menu" href="prototype.php">the Prototype</a></td>
@@ -30,7 +30,7 @@ $footer = '<!-- footer -->
 				<div class = "aboutButtons"><p>PDP</p></div>
 				<div class = "aboutButtons"><p>Aalto University</p></div>
 				<div class = "aboutButtons"><p>Design Factory</p></div>
-				<a href="https://www.facebook.com"><img src="images/icons/facebookicon2.png"; height="80px"; width = "100px"/></a>
-				<a href="https://www.facebook.com"><img src="images/icons/emailicon.png" ; height="100px"; width = "100px"/></a>
+				<a href="https://www.facebook.com"><img src="images/icons/fb.png"; height="45px"; width = "45x"/></a>
+				<a href="mailto:adress@server.com?subject=Hello Team That Loves People"><img src="images/icons/email.png" ; height="45px"; width = "45px""/></a>
 			</div>';
 ?>
