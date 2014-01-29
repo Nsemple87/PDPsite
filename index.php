b86a68c3fe9abd40dd15e26d424bf67bb4454204
Shane <droidburgundy@gmail.com>
2014-01-29 13:26:45 -0500

    html5 all the things

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
 <?php
 require_once 'core/init.php';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="UTF-8">
 <?php echo $css?>
 <title>Team That Loves People</title>
 </head>
-
 <body>
-
 <?php echo $header ?>
-
 <div class= "fullpage" id="midway">
-
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 		<script>
 			$(function(){
@@ -23,7 +17,6 @@ require_once 'core/init.php';
 			setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
 			});
 		</script>
-
 		<div class="fadein" id="slider">
 			<img src="images/slider/01.jpg">
 			<img src="images/slider/02.jpg">
@@ -31,13 +24,7 @@ require_once 'core/init.php';
 			<img src="images/slider/04.jpg">
 			<img src="images/slider/05.jpg">
 		</div>
-
 </div>
-
-
-
-
 <?php echo $footer ?>
-
 </body>
 </html>
--- press.php
+++ press.php
@@ -1,20 +1,14 @@
 <?php
 require_once 'core/init.php';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="UTF-8">
 <?php echo $css?>
 <title>the PRESS</title>
 </head>
 <body>
-
 <?php echo $header ?>
-
-
-
 <?php echo $footer ?>
-
 </body>
 </html>
