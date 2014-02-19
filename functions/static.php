d27a9aef00a1cf8ad042501082285858b51262a2
Shane <droidburgundy@gmail.com>
2014-02-19 01:40:43 -0500

    GET SOME!!
    I don’t want any sweedish fish

--- functions/static.php
+++ functions/static.php
@@ -2,54 +2,57 @@
 /*
 Blocks of static HTML etc so we don't have to edit things multiple times, store header, navbar etc - items which are on every page
 */
+$header = '
+<div id="wrap"><!-- Wrap all page content here -->
 
-$ie_html5 = '<!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->';
-
-//emersive full background imagery idea taken from: http://css-tricks.com/perfect-full-page-background-image/
-$css = '<!-- CSS -->
-		<style>
-			* { margin: 0; padding: 0; }
-			
-			html { 
-				background: no-repeat center center fixed; 
-				-webkit-background-size: cover;
-				-moz-background-size: cover;
-				-o-background-size: cover;
-				background-size: cover; }
-
-			html, body {
-				height: 100%; }
-		</style>
-		<link type="text/css" rel="stylesheet" href="css/resetstylesheet.css"/>
-	    <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/><!-- CSS -->';
-
-$header = '<!-- Navigation bar -->
-			<div class="wrapper">
-				<div id="navbar">  
-					<nav>
-						<table>
-							<tr>
-								<td>&nbsp;</td>
-							</tr>
-							<tr>
-								<td><a class="logo" href="index.php"><img src="images/icons/logo.gif" width="190" height="105"></a></td>
-		                        <td><a class="menu" href="project.php">the Project</a></td>
-				                <td><a class="menu" href="people.php">the People</a></td>
-				                <td><a class="menu" href="prototype.php">the Prototype</a></td>
-				                <td><a class="menu" href="press.php">the Press</a></td>
-				            </tr>
-				        </table>
-				    </nav>
-				</div>';
+<!-- NAVBAR -->
+<div class="navbar-wrapper">
+<div class="container">
+<nav class="navbar-wrapper navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand navbar-left" href="#">TEAM THAT <3 PEOPLE</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->          
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+              <li class="disabled"><a href="#"><span class="glyphicon glyphicon-heart-empty"></span> Project</a></li>
+              <li class="dropdown">  
+               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> People<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li class="disabled"><a href="#">Meet the Team</a></li>
+                  <li class="disabled"><a href="#">Contact Us</a></li>
+                </ul>
+              </li>
+              <li class="disabled"><a href="#"><span class="glyphicon glyphicon-bullhorn"></span> Press</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+        </nav>
+    </div><!-- /container -->
+    </div><!-- /navbar wrapper -->';
 
 //Footer Logic/code taken from: http://ryanfait.com/resources/footer-stick-to-bottom-of-page/
-$footer = '<div class="push"></div>
-    	 </div>
-    	 <div id="footer" class="footer">
-    	 	<hr>
-			<a href="booyah.php"><img src="images/icons/fb.png"; height="45px"; width = "45x"/></a>
-			<a href="mailto:adress@server.com?subject=Hello Team That Loves People"><img src="images/icons/email.png" ; height="45px"; width = "45px""/></a>
-			<p><center><strong>Copyright © 2014. Team That Loves People. All rights reserved.</strong></center></p>
-         </div>';
+$footer = '
+</div><!--/container-->
+</div><!--/wrap-->
+<div id="footer">
+  <div class="container">
+  <div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8"><p class="text-muted" align="center">Copyright &copy; 2014. Team That Loves People. All rights reserved.</p>
+    </div>
+    <div class="col-md-2"><a href="booyah.php"><span class="glyphicon glyphicon glyphicon-heart"></span></a></div>
+
+    
+  </div>
+</div>';
 
 ?>
--- people.php
+++ people.php
@@ -2,14 +2,22 @@
 require_once 'core/init.php';
 ?>
 <!DOCTYPE html>
+<html lang="en">
+<!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
 <head>
-<?php echo $ie_html5;?>
-<meta charset="UTF-8">
-<?php echo $css;?>
-<title>the PEOPLE</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<title>The People</title>
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
 </head>
 <body>
+
 <?php echo $header;?>
+
 <?php echo $footer;?>
+
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src="scripts/bootstrap.js"></script>
 </body>
 </html>
