4822d03a18e77a4d9c0259936d69b16e62cf1581
Shane <droidburgundy@gmail.com>
2014-03-04 15:00:11 -0500

    added htaccess

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require_once 'core/init.php';
 <!DOCTYPE html>
 <html lang="en">
 <!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+
 <head>
 <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Team That Loves People</title>
 <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-
 </head>
 <body>
 
