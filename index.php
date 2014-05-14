a2c883e8e90e4927262213a884935601168927be
Nicole <ns21549n@pace.edu>
2014-05-14 14:50:24 -0400

    css changes

--- index.php
+++ index.php
@@ -1,4 +1,55 @@
-<?php require ("config/config.php"); require (HEAD);?>
+<!DOCTYPE html>
+<html lang="en">
+<!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+<head>
+<base href="/">
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Hospital Model Space: Meilahti New Children Hospital</title>
+<link rel="shortcut icon" href="pdpsite/public/img/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="pdpsite/public/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="pdpsite/public/css/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="pdpsite/public/css/custom.css"/>
+</head>
+<body>
+<div id="wrap"><!-- Wrap all page content here -->
+<!-- NAVBAR -->
+<div class="navbar-wrapper">
+<div class="container">
+<nav class="navbar-wrapper navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand navbar-left" href="/">Hospital Model Space Project</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Project<b class="caret"></b></a>
+                 <ul class="dropdown-menu">
+                  <li class="disabled"><a href="#">Model space</a></li>
+                  <li class="disabled"><a href="#">1:10 scale model</a></li>
+                  <li class="disabled"><a href="#">New ideas</a></li>
+                </ul>
+                </li>
+              <li class="dropdown">  
+               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> People<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li class="disabled"><a href="#">Meet the Team</a></li>
+                  <li class="disabled"><a href="#">Contact Us</a></li>
+                </ul>
+              </li>
+              <li class="disabled"><a href="#"><span class="glyphicon glyphicon-bullhorn"></span> Press</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+        </nav>
+    </div><!-- /container -->
+    </div><!-- /navbar wrapper -->
 <!-- Carousel -->
 <div id="myCarousel" class="carousel slide">
   <!-- Indicators -->
@@ -10,7 +61,7 @@
   <div class="carousel-inner">
 
     <div class="item active">
-      <img src="images/slider/1.jpg">
+      <img src="pdpsite/public/img/slider/1.jpg">
       <div class="container">
         <div class="carousel-caption">
           <h1>CHILDREN’S HOSPITAL MODEL SPACE</h1>
@@ -20,7 +71,7 @@
     </div>
 
     <div class="item">
-      <img src="images/slider/2.jpg">
+      <img src="pdpsite/public/img//slider/2.jpg">
       <div class="container">
         <div class="carousel-caption">
           <h1>Working Together</h1> 
@@ -30,7 +81,7 @@
     </div>
 
     <div class="item">
-      <img src="images/slider/5.jpg">
+      <img src="pdpsite/public/img//slider/5.jpg">
       <div class="container">
         <div class="carousel-caption">
           <h1>Comfy?</h1>
@@ -62,20 +113,20 @@
 <div class="container-fluid">
           <div class="row">
             <div class="col-md-4">
-              <a href=""><img class="img-responsive img-circle" src="images/circle_1_lifesizemodel.png"></a>
+              <a href=""><img class="img-responsive img-circle" src="pdpsite/public/img/circle_1_lifesizemodel.png"></a>
               <h3 class="text-center"><a href=""id = "text">Model space</a></h3>
               <p><i>The actual-sized model room is used as a tool for designing and achieving the perfect setup and functionality for a high-dependency hospital room. Co-design methods are being used to ensure that every user group is taken into account.</i></p>
               <p>After the preliminary field research, finding out about the nurse’s and patients lives in the hospital, we started running the model spaces, first with each other and then the actual demo run with the nurses. We started the event by making all the items and devices necessary to the space. In order to achieve a full list of items to be placed in the high dependency room, we did several field trips to the hospital complemented with nurse interviews. For the first prototype of the model room, we used cardboard and other easy-to-build materials. Later, we started to bring in real equipment to capture a more realistic environment. Currently, the 1:1 sized model room is being tested by different user groups. The main focus of the test runs are to find the best design for the model room, while the secondary focus will be on innovating and ideating about the structure and functionality of the real hospital room itself. The user groups include doctors, nurses, hospital staff and patients along their family. This co-design method will ensure that every point of view is taken into consideration when designing and planning the real hospital room. Improvements to the model space are also being made after each test run.</p>
             </div>
  
             <div class="col-md-4">
-              <a href=""><img class="img-responsive img-circle" src="images/circle_2_110model.png"></a>
+              <a href=""><img class="img-responsive img-circle" src="pdpsite/public/img/circle_2_110model.png"></a>
               <h3 class="text-center"><a href=""id = "text">1:10 scale model</a></h3>
               <p><i>The 1:10 model is a design tool that is used to complement the actual-sized model. When brainstorming, the small scale model allows us to quickly test out new ideas. This is really effective since any big changes done to the real-sized model will be more time consuming.</i></p>
               <p>The 1:10 model allows us to brainstorm on ideas and quickly test them out whenever and wherever we are. It is not bound to a specific location, which means we do not always have to visit the actual-sized model to test our ideas. This is essential when working with other partners who contribute to the project but are not a part of our team. The small-scale model also lets us quickly test new ideas. Any changes and testing with the actual-sized model space is always time consuming. With the help of the 1:10 model we can test and generate more ideas in less time. The small-scale model is also used in the co-design workshops that we have arranged in the 1:1 model space. It helps the participants who are unfamiliar with the model space and project to quickly get a general idea and grasp of what we are trying to achieve.</p>
               </div>
             <div class="col-md-4">
-              <a href=""><img class="img-responsive img-circle" src="images/circle_3_newideas.png"></a>
+              <a href=""><img class="img-responsive img-circle" src="pdpsite/public/img/circle_3_newideas.png"></a>
               <h3 class="text-center"><a href=""id = "text">New ideas</a></h3>
               <p><i>Doing extensive research is essential when working with any design task, more so when you’re unfamiliar with the field you’re supposed to work in. This is why we devoted a great deal of time in understanding the details of the bigger picture.</i></p>
               <p>To fully understand our task, we needed to do research on everything from visiting a hospital, to the devices and procedures used. We started our research by investigating the patient journey. The patient journey was made by interviewing three nurses at the current Children’s Hospital. It helped us to form an idea of what the patient goes through during his or her visit to the hospital.</p>
